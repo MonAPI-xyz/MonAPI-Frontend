77e19d92d98c76106c52e758bff4cc69335c8b7f
“HugoIr” <hugoirwanto23@gmail.com>
2022-09-23 17:11:02 +0700

    [CHORES] add font and chakra ui theme

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,16 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
-
-// Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
 import Login from '../routes/login';
 import { ChakraProvider } from '@chakra-ui/react';
-// import { store, useGlobalState } from 'preact-global-state';
+import theme from '../config/theme';
 import { Provider } from 'unistore/preact';
 import { store } from '../config/store/store.js';
 
 const App = () => {
 	return (<div id="app">
 		<Provider store={store}>
-			<ChakraProvider>
+			<ChakraProvider theme={theme}>
 				<Router>
 					<Home path="/" />
 					<Login path="/login" />
---
+++ src/config/theme/index.js
@@ -0,0 +1,20 @@
+import { extendTheme, theme as base } from '@chakra-ui/react';
+
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
+  fonts: {
+    heading: `Open Sans, ${base.fonts?.heading}`,
+    body: `Open Sans, ${base.fonts?.body}`,
+  },
+  breakpoints: {
+    xs: '10em',
+    sm: '30em',
+    md: '48em',
+    lg: '62em',
+    xl: '80em',
+    '2xl': '96em',
+    '4xl': '118em',
+  },
+});
+
+export default theme;
