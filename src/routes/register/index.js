f412c549f5b8797cd39217976b6bd2e048ea5065
Lucky Susanto <luckyblock0@gmail.com>
2022-09-30 15:33:07 +0700

    [GREEN] User must enter valid email and password, else show errors

--- src/routes/register/index.js
+++ src/routes/register/index.js
@@ -1,19 +1,56 @@
 import TextInput from '../../components/forms/textinput/index.js'
 import { h } from "preact";
+import { useState } from "preact/hooks";
 import PasswordInput from '../../components/forms/passwordinput/index.js'
 import { Button, Text  } from '@chakra-ui/react';
 import {Box, Flex, Grid, GridItem } from '@chakra-ui/layout';
 import { useForm } from 'react-hook-form';
+import * as axios from "axios";
 
 function Register() {
     console.log("Entering Register")
+    
+    const [errl, setErrl] = useState([])
+
     const {
       handleSubmit,
       register,
       formState: { errors },
     } = useForm();
 
-    const onSubmit = () => {};
+    const onSubmit = (res) => {
+        const data = {
+            email: res.email,
+            password: res.password,
+            password2: res.password2
+        }
+
+        axios.post(`https://api-staging.monapi.xyz/register/api`, data)
+        .then(data => {
+            console.log("Entering then")
+        }) 
+        .catch((error) => {
+            let error_logs = []
+            if (error.response.data.email) {
+                const email_error = error.response.data.email
+                error_logs = error_logs.concat(email_error)
+            }
+            if (error.response.data.password) {
+                let password_error = error.response.data.password
+                if (password_error[0].startsWith('[')){
+                    password_error = error.response.data.password[0].split("', '")
+                    password_error[0] = password_error[0].slice(2);
+                    password_error[password_error.length - 1] = password_error[password_error.length - 1].slice(0, -2);
+                }
+                error_logs = error_logs.concat(password_error)
+            }
+            if (error_logs){
+                setErrl(error_logs)
+            }
+        });
+
+        return data
+    }
 
     return         (
     <Flex
@@ -38,6 +75,9 @@ function Register() {
                         placeholder='Your Email'
                         errors={errors}
                         register={register}
+                        rules = {{
+                            required: "Required"
+                        }}
                     />
                     <Box mb='20px'/>
                     
@@ -47,26 +87,44 @@ function Register() {
                         placeholder='Password'
                         errors={errors}
                         register={register}
+                        rules = {{
+                            required: "Required"
+                        }}
                     />
                     <Box mb='20px'/>
 
                     <PasswordInput
                         id="password2"
-                        title="Password2"
+                        title="Confirm Password"
                         placeholder='Confirm Password'
                         errors={errors}
                         register={register}
+                        rules = {{
+                            required: "Required"
+                        }}
                     />
                     <Box mb='20px'/>
+
+                    {errl.length != 0 && (
+                        <div>
+                        <Text color='red'>Error:</Text>
+                        <ul data-testid='errl'>
+                            {errl.map(e => (
+                                <li>{e}</li>
+                            ))}
+                        </ul>
+                        </div>
+                    )}
+                    <Box mb='20px' />
                     <Button id='signInButton' colorScheme='teal' type='submit' width='12em' borderRadius={10}>
                         Sign Up
                     </Button>
                 </Box>
                 
                 <Box mb='20px'/>
-                <Text as='span'>or </Text>
+                <Text as='span'>Already have an account? </Text>
                 <Text as='u' fontWeight='600'>    
-                    Have an account? <Text as='span' color='#4B8F8C'>sign in</Text> to login.
+                    <Text as='span' color='#4B8F8C'>sign in</Text>
                 </Text>
             </Box>
                                 
