11250f5f1df04307514537870de6d7ec680ba751
ferdifdi <ferdifadillah3@gmail.com>
2022-10-10 13:29:55 +0700

    [FIX] fix bug & to && operator for conditional

--- src/components/success_rate/index.js
+++ src/components/success_rate/index.js
@@ -4,7 +4,7 @@ import style from './style.css';
 const SuccessRate = ({success,failed})=> {
     return (
         <>
-            {(success == 0 & failed == 0)?
+            {(success == 0 && failed == 0)?
                 <div class={style['grey-bar']}>
                     
                 </div>
