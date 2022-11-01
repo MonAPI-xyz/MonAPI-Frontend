8ab808e76e2bd3eef24fe8734c68f4dac75ec85b
Muhammad Fahlevi <muhammad.fahlevi@gdn-commerce.com>
2022-11-01 22:38:20 +0700

    [RED] create tests for threshold configuration and remove redundant test

--- tests/routes/configuration/configuration.test.js
+++ tests/routes/configuration/configuration.test.js
@@ -1,13 +1,11 @@
-import { h, Fragment } from 'preact';
-import Login from '../../../src/routes/login/index.js';
-import { Provider } from 'unistore/preact';
-import { store } from '../../../src/config/store/store';
+import { h } from 'preact';
 import { screen, waitFor, render } from '@testing-library/preact';
 import * as axios from 'axios';
 import { getCurrentUrl, route } from 'preact-router';
 import App from '../../../src/components/app.js';
+import Configuration from '../../../src/routes/configuration/index.js'
 import userEvent from '@testing-library/user-event'
-import { deleteUserToken, setUserToken } from '../../../src/config/api/auth.js';
+import { setUserToken } from '../../../src/config/api/auth.js';
 
 jest.mock("axios");
 
@@ -23,6 +21,7 @@ describe('Test configuration sites', () => {
 			expect(getCurrentUrl()).toBe('/configuration/');
 		})
 	})	
+	
 	test('test save config', async () => {
 		const response = {
 			"is_slack_active": false,
@@ -35,13 +34,16 @@ describe('Test configuration sites', () => {
 			"is_pagerduty_active": false,
 			"pagerduty_api_key": "",
 			"pagerduty_default_from_email": "",
+			"pagerduty_service_id": "",
 			"is_email_active": false,
 			"email_host": "",
 			"email_port": null,
 			"email_username": "",
 			"email_password": "",
 			"email_use_tls": true,
-			"email_use_ssl": true
+			"email_use_ssl": true,
+			"threshold_pct": 100,
+			"time_window":'1H',
 		}
 		axios.get.mockImplementation(() => Promise.resolve({data: response}))
 		setUserToken("token")
@@ -54,10 +56,11 @@ describe('Test configuration sites', () => {
 		axios.post.mockImplementation(()=>Promise.resolve({data: response}))
 
 		await waitFor(() => {
-			expect(screen.getByText("successful save configuration"))
+			expect(screen.getByText("Successful save configuration"))
 			expect(getCurrentUrl()).toBe('/configuration/');
 		})
 	})	
+
 	test('test save config loading', async () => {
 		const response = {
 			"is_slack_active": false,
@@ -70,13 +73,16 @@ describe('Test configuration sites', () => {
 			"is_pagerduty_active": false,
 			"pagerduty_api_key": "",
 			"pagerduty_default_from_email": "",
+			"pagerduty_service_id": "",
 			"is_email_active": false,
 			"email_host": "",
 			"email_port": null,
 			"email_username": "",
 			"email_password": "",
 			"email_use_tls": true,
-			"email_use_ssl": true
+			"email_use_ssl": true,
+			"threshold_pct":100,
+			"time_window":'1H',
 		}
 		axios.get.mockImplementation(() => Promise.resolve({data: response}))
 		setUserToken("token")
@@ -100,6 +106,7 @@ describe('Test configuration sites', () => {
 
 		userEvent.click(saveButton);
 	})	
+
 	test('test save config failed', async () => {
 		const response = {
 			"is_slack_active": false,
@@ -112,13 +119,16 @@ describe('Test configuration sites', () => {
 			"is_pagerduty_active": false,
 			"pagerduty_api_key": "",
 			"pagerduty_default_from_email": "",
+			"pagerduty_service_id": "",
 			"is_email_active": false,
 			"email_host": "",
 			"email_port": null,
 			"email_username": "",
 			"email_password": "",
 			"email_use_tls": true,
-			"email_use_ssl": true
+			"email_use_ssl": true,
+			"threshold_pct":100,
+			"time_window":'1H',
 		}
 		axios.get.mockImplementation(() => Promise.resolve({data: response}))
 		setUserToken("token")
@@ -135,17 +145,52 @@ describe('Test configuration sites', () => {
 			expect(getCurrentUrl()).toBe('/configuration/');
 		})
 	})	
-	test('test form', async () => {
-		const response = []
+
+	test('when do wrong input on Threshold, then appears error message', async () => {
+		const response = {
+			"is_slack_active": false,
+			"slack_token": "",
+			"slack_channel_id": "",
+			"is_discord_active": false,
+			"discord_bot_token": "",
+			"discord_guild_id": "",
+			"discord_channel_id": "",
+			"is_pagerduty_active": false,
+			"pagerduty_api_key": "",
+			"pagerduty_default_from_email": "",
+			"pagerduty_service_id": "",
+			"is_email_active": false,
+			"email_host": "",
+			"email_port": null,
+			"email_username": "",
+			"email_password": "",
+			"email_use_tls": true,
+			"email_use_ssl": true,
+			"threshold_pct":100,
+			"time_window":'1H',
+		}
 		axios.get.mockImplementation(() => Promise.resolve({data: response}))
 		setUserToken("token")
-		render(<App/>);
-		route('/configuration/')
+		render(<Configuration/>);
 
+		const saveButton = screen.getByText("Save")
+
+		const thresholdAccordion = screen.getByText("Threshold")
+		userEvent.click(thresholdAccordion);
+		
+		const thresholdValueInput = screen.getByPlaceholderText("Insert the integer value from 1 to 100")
+		
+		userEvent.clear(thresholdValueInput)
+		userEvent.click(saveButton);
 		await waitFor(() => {
-			expect(screen.getByText('Token'))
-			expect(screen.getByPlaceholderText('Insert your slack token'))			
-			expect(getCurrentUrl()).toBe('/configuration/');
+			expect(screen.getByText("Required"))
+		})
+
+		userEvent.clear(thresholdValueInput)
+		userEvent.type(thresholdValueInput, '90.5')
+		userEvent.click(saveButton);
+		await waitFor(() => {
+			expect(screen.getByText("Value must be integer"))
 		})
 	})
 });
