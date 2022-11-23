1ba0dba6fdc97dd59ac01bd00b3d63a684c367d5
ferdifdi <ferdifadillah3@gmail.com>
2022-11-23 16:41:55 +0700

    [RED] test view current team

---
+++ tests/routes/view_current_team/view_current_team.test.js
@@ -0,0 +1,56 @@
+import { h } from 'preact';
+import { render, waitFor, screen, fireEvent } from '@testing-library/preact';
+import ViewCurrentTeam from '../../../src/routes/view_current_team';
+import userEvent from '@testing-library/user-event'
+import * as axios from 'axios';
+import { setUserToken } from '../../../src/config/api/auth.js';
+
+jest.mock('axios')
+
+describe('Test View Current Team', () => {
+  test('try to view current team', async () => {
+		const response = {
+        "id": 5,
+        "name": "Admin2",
+        "logo": null,
+        "description": "Test desc",
+        "teammember": [
+            {
+                "team": 5,
+                "user": {
+                    "id": 6,
+                    "username": "admin2@admin.com",
+                    "email": "admin2@admin.com",
+                    "first_name": "",
+                    "last_name": ""
+                },
+                "verified": true
+            },
+            {
+              "team": 5,
+              "user": {
+                  "id": 7,
+                  "username": "admin3@admin.com",
+                  "email": "admin3@admin.com",
+                  "first_name": "",
+                  "last_name": ""
+              },
+              "verified": false
+            }
+        ]
+    }
+		axios.get.mockImplementation(() => Promise.resolve({data: response}))
+		setUserToken("token")
+		render(<ViewCurrentTeam/>);
+
+		await waitFor(() => {
+      expect(screen.getByText('Team Management'))
+      expect(screen.getByText('Admin2'))
+      expect(screen.getByText('Test desc'))
+      expect(screen.getByText('admin2@admin.com'))
+      expect(screen.getByText('Member'));
+      expect(screen.getByText('Pending'));
+		})
+	})
+
+})
