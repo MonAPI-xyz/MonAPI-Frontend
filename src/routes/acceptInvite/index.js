cce46be89a547c9dc36825fe35a9a8075f09f793
Lucky Susanto <78904597+Exqrch@users.noreply.github.com>
2022-11-26 19:45:23 +0700

    feat: Pbi 27 invite team member frontend (#82)
    * [RED] Create Test for redirect invite team feature
    
    * [GREEN] Implement page for redirect invite team feature
    
    * [RED] TeamMemberComponent test is Created
    
    * [GREEN] TeamMemberComponent implemented
    
    * [REFACTOR] Clean Code

---
+++ src/routes/acceptInvite/index.js
@@ -0,0 +1,116 @@
+import { h } from "preact";
+import { useState } from 'preact/hooks';
+import { useEffect } from "react";
+import { Box, Heading, Text, Flex, Spinner } from '@chakra-ui/react';
+import { CheckCircleIcon, CloseIcon } from '@chakra-ui/icons';
+import BASE_URL from '../../config/api/constant.js';
+import axios from "axios";
+
+const AcceptInvite = () => {
+
+    const paramInviteToken = new URLSearchParams(window.location.search).get('key')
+    const [inviteToken, setInviteToken] = useState(null)
+    const [response, setResponse] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(async () => {
+        if (inviteToken != null) {
+            const data = {
+                'key': inviteToken
+            }
+            axios.post(`${BASE_URL}/invite-member/accept/`, data)
+            .then((backend_response) => {
+                setIsLoading(false)
+                setResponse(backend_response.response.data)
+            })
+            .catch((error) => {
+                setIsLoading(false)
+                setResponse(error.response.data)
+            })
+        }
+        
+    }, [inviteToken])
+
+    if (paramInviteToken){
+        setInviteToken(paramInviteToken);
+    } else {
+        return (
+            <Box textAlign="center" py={10} px={6}>
+                <Box display="inline-block">
+                <Flex
+                    flexDirection="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    bg={'red.500'}
+                    rounded={'50px'}
+                    w={'55px'}
+                    h={'55px'}
+                    textAlign="center">
+                    <CloseIcon boxSize={'20px'} color={'white'} />
+                </Flex>
+                </Box>
+                <Heading as="h2" size="xl" mt={6} mb={2}>
+                    Token Not Passed
+                </Heading>
+                <Text color={'gray.500'}>
+                    You are not supposed to see this page.
+                </Text>
+            </Box>
+        )
+    }
+
+    if (isLoading) {
+        return (
+        <Box textAlign="center" py={10} px={6}>
+            <Text color={'gray.500'}>Loading</Text>
+            <Spinner />
+            <Text color={'gray.500'}>We are Accessing Your Request</Text>
+        </Box>
+        )
+    }
+
+    if (response.success) {
+        return (
+            <Box textAlign="center" py={10} px={6}>
+            <CheckCircleIcon boxSize={'50px'} color={'green.500'} />
+            <Heading as="h2" size="xl" mt={6} mb={2}>
+                Team Invite Accepted
+            </Heading>
+            <Text color={'gray.500'}>
+                You are now a part of a new team!
+                Login to work with everyone else now.
+            </Text>
+            </Box>
+        )
+    }
+
+    if (response.error != null) {
+        return (
+        <Box textAlign="center" py={10} px={6}>
+            <Box display="inline-block">
+            <Flex
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                bg={'red.500'}
+                rounded={'50px'}
+                w={'55px'}
+                h={'55px'}
+                textAlign="center">
+                <CloseIcon boxSize={'20px'} color={'white'} />
+            </Flex>
+            </Box>
+            <Heading as="h2" size="xl" mt={6} mb={2}>
+                Token Invalid
+            </Heading>
+            <Text color={'gray.500'}>
+                It seems like the token has expired or the invite was cancelled. <br></br>
+                Please request another invite from your Team. <br></br>
+                Error = {response.error}
+            </Text>
+        </Box>
+        )
+    }
+}
+
+export default AcceptInvite;
---
+++ tests/components/teamMemberComponent/teamMemberComponent.test.js
@@ -0,0 +1,59 @@
+import { h } from "preact";
+import { render, fireEvent, waitFor, screen } from '@testing-library/preact';
+import TeamMemberComponent from '../../../src/components/teamMemberComponent';
+import axios from "axios";
+
+jest.mock('axios');
+describe('Test team member component', () => {
+    test('when header is true, set up header', async() =>{
+        render(<TeamMemberComponent header={true}/>)
+
+        await waitFor(async () => {
+            expect(screen.getByText('Members')).toBeDefined()
+            expect(screen.getByText('Status')).toBeDefined()
+        })
+    })
+
+    test('when verified is true, status is member', async() =>{
+        render(<TeamMemberComponent email={`test@gmail.com`} verified={true}/>)
+
+        await waitFor(async () => {
+            expect(screen.getByText('member')).toBeDefined()
+            expect(screen.getByText('test@gmail.com')).toBeDefined()
+        })
+    })
+
+    test('when verified is false, status is pending', async() => {
+        render(<TeamMemberComponent email={'test@gmail.com'} verified={false} />)
+
+        await waitFor(async () => {
+            expect(screen.getByText('pending')).toBeDefined()
+            expect(screen.getByText('test@gmail.com')).toBeDefined()
+        })
+    })
+
+    test('when verified is false, status is pending', async() => {
+        const container = render(<TeamMemberComponent email={'test@gmail.com'} verified={false} cancelUserId={1}/>)
+
+        axios.post.mockImplementation(() => Promise.resolve({
+            status: 200,
+            response: {
+                data: {
+                    success: true
+                }
+            }
+        }))
+
+        delete window.location;
+        window.location = { reload: jest.fn() };
+
+        const cancelButton = screen.getByRole('button')
+        fireEvent.click(cancelButton)
+
+
+        await waitFor(async () => {
+            expect(window.location.reload).toHaveBeenCalled()
+        })
+    })
+
+})
