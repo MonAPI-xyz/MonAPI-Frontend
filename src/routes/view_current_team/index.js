39fd569054466a9739014b2d1eef71c0fe54931f
ferdifdi <ferdifadillah3@gmail.com>
2022-11-27 15:10:13 +0700

    [REFACTOR] No need bootstrap

--- src/routes/view_current_team/index.js
+++ src/routes/view_current_team/index.js
@@ -24,9 +24,6 @@ const ViewCurrentTeam = () => {
     
     return (
         <div style={{padding: "40px 20px"}}>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous" />
-			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" />
-
             <Flex
                 h="full"
                 backgroundRepeat='no-repeat'
