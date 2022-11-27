2cc9c6293cc0f5fe72d302a1084d06d8c62f1730
Lucky Susanto <78904597+Exqrch@users.noreply.github.com>
2022-11-27 21:27:08 +0700

    feat: PBI 27 invite team member frontend input (#86)
    * [RED] Create test for invite team member section
    
    * [GREEN] Team member invite bar implemented
    
    * [REFACTOR] Add space from form input to button
    
    * [REFACOTR] Code smells

--- src/routes/view_current_team/index.js
+++ src/routes/view_current_team/index.js
@@ -1,5 +1,6 @@
 import { h } from 'preact';
-import { Text, Box, Flex, Grid, GridItem } from '@chakra-ui/react';
+import { Text, Box, Flex, Grid, GridItem, Button } from '@chakra-ui/react';
+import TextInput from '../../components/forms/textinput/index.js'
 import { useState, useEffect } from 'preact/hooks';
 import axios from 'axios';
 import BASE_URL from '../../config/api/constant.js';
@@ -7,6 +8,8 @@ import { getUserToken } from '../../config/api/auth.js';
 import { Link } from 'preact-router/match'
 import logo from '../../assets/icons/logo-monapi.svg';
 import TeamMemberComponent from '../../components/teamMemberComponent/index.js';
+
+import { useForm } from 'react-hook-form';
 
 const ViewCurrentTeam = () => {        
     const [currentTeam,setCurrentTeam]=useState([])
@@ -20,6 +23,37 @@ const ViewCurrentTeam = () => {
 		})
 	},[])
 
+    // Invite Form
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm();
+    const [extraError, setExtraError] = useState(null);
+
+    const sendInvite = (res) => {
+
+        const data = {
+            invited_email: res.invited_email
+        }
+        axios.post(`${BASE_URL}/invite-member/token/`, data, {
+            headers: {
+                Authorization: `Token ${getUserToken()}`
+            }
+        }).then(() => {
+            window.location.reload()
+        }).catch((error) => {
+            setExtraError(error)
+        })
+    }
+
+    const listErrors = (error_list) => {
+        if (error_list != null) {
+            return (
+                <Text color="red">Error: Make sure the user exist or isn't already in the team</Text>
+            )
+        }
+    }
     
     return (
         <div style={{padding: "40px 20px"}}>
@@ -78,7 +112,27 @@ const ViewCurrentTeam = () => {
                             </div>                                                          
                             </div>    
                             
-                            <Box mb='20px' />
+                            <Box mb='40px' />
+                            
+                            <Box as='form' onSubmit={handleSubmit(sendInvite)}>
+                                <Flex>
+                                    <TextInput 
+                                        id="invited_email"
+                                        title='Invite Member' 
+                                        placeholder='example@email.com'
+                                        errors={errors}
+                                        register={register}
+                                        rules = {{
+                                            required: "Required"
+                                        }}
+                                    />
+                                </Flex>
+                                <Box mb="20px"/>
+                                <Button data-testid='test-inviteButton' id='inviteButton' colorScheme='teal' type='submit' borderRadius={10}>
+                                        Invite
+                                </Button>
+                                {listErrors(extraError)}
+                            </Box>
 
                             <Text mb={23} fontSize='32px' fontWeight='semibold' color='black'>
                                 Team Members List
