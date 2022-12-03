5b1df1f4a3753c8ba0b524ebe5089ffd41b456df
Lucky Susanto <luckyblock0@gmail.com>
2022-12-03 17:48:58 +0700

    [RED] Alert user to verify after register

--- tests/routes/login/login.test.js
+++ tests/routes/login/login.test.js
@@ -182,7 +182,7 @@ describe('Test Form Login', () => {
 		route('/login?isRegistered=true')
 
 		await waitFor(async () => {
-            expect(screen.getByText('User Created Successfully!'))
+            expect(screen.getByText('Please verify your account before logging-in by checking your email.'))
 		})
 	})
 
