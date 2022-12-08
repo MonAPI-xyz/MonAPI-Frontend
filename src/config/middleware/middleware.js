996be585cdb15e1abddd503ba2a63d240d3b4026
Andrew <susanto.andrew@ymail.com>
2022-12-08 19:34:36 +0700

    fix: fix redirect authentication (#103)

--- src/config/middleware/middleware.js
+++ src/config/middleware/middleware.js
@@ -1,10 +1,19 @@
 import { h } from "preact"
 import { route } from "preact-router"
+import { useEffect } from "react"
 import { getUserToken } from "../api/auth"
 import ROUTE from "../api/route"
 
 const AuthenticationChecker = ({children}) => {
-    return isAuthenticate() ? children : route(ROUTE.LOGIN)
+    const isAuthenticated = isAuthenticate();
+
+    useEffect(()=>{
+        if (!isAuthenticated) {
+            route(ROUTE.LOGIN, true)
+        }
+    }, [isAuthenticated])
+
+    return isAuthenticated ? children : <div />
 }
 
 function isAuthenticate() {
